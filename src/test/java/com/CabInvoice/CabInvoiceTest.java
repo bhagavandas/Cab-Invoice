933705cce6f234e1b0f686dfaeb31b1ecaea1221
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-08 10:54:12 +0530

    Added: Testcase and multiple trip fare calculation

--- src/test/java/com/CabInvoice/CabInvoiceTest.java
+++ src/test/java/com/CabInvoice/CabInvoiceTest.java
@@ -1,19 +1,37 @@
 package com.CabInvoice;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 public class CabInvoiceTest {
-
+	
 	@Test
 	public void givenDistanceAndTimeWhenFareIsFiveShouldBeTotalFare() {
-
+		
 		CabInvoiceMain cab = new CabInvoiceMain();
 		int result = cab.calculateFare(10);
+		//int totalfare = cab.totalFare;
 		Assert.assertEquals(50, result);
+		
 	}
 	
-	
+	@Test
+	public void givenDistanceAndTimeWhenFareIsFiveForMultipleRidesShouldBeMultipleTotalFare() {
+		
+		CabInvoiceMain cab = new CabInvoiceMain();
+		Scanner scan = new Scanner(System.in);
+		
+		int result = cab.calculateFare(10);
+		System.out.println("Enter number of trips: ");
+		int trips = scan.nextInt();
+		int totalFare = result * trips;
+		//int totalfare = cab.totalFare;
+		Assert.assertEquals(totalFare, result*trips);
+		System.out.println(totalFare);
+		
+	}
 
 }
